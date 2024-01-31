4896e50a5e561de9a6c67e1e86f76899469c5966
devbwoh <bwoh@outlook.kr>
2024-02-01 06:46:44 +0900

    🎴 Card 사용

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet20retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet20retrofit/MainActivity.kt
@@ -3,14 +3,20 @@ package kr.ac.kumoh.ce.prof01.jet20retrofit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.ac.kumoh.ce.prof01.jet20retrofit.ui.theme.Jet20RetrofitTheme
@@ -45,8 +51,39 @@ fun SongList() {
             items = viewModel.songs,
             key = { song -> song.id },
         ) {
-            Text(it.toString(), fontSize = 20.sp)
+            SongItem(it)
         }
     }
 }
 
+@Composable
+fun SongItem(song: Song) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(8.dp),
+    ) {
+        Column(
+            Modifier.padding(8.dp)
+        ) {
+            SongTitle(song.title)
+            SingerName(song.singer)
+        }
+    }
+}
+
+@Composable
+fun SongTitle(title: String) {
+    Text(
+        text = title,
+        modifier = Modifier.fillMaxWidth(),
+        fontSize = 20.sp,
+        lineHeight = 25.sp,
+    )
+}
+
+@Composable
+fun SingerName(singer: String) {
+    Text(singer)
+}
