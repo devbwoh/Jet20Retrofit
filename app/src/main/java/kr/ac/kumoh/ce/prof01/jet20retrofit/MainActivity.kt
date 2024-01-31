ce9e9dc5f394198cb54020b4c602fc274609b2fb
devbwoh <bwoh@outlook.kr>
2024-02-01 06:57:47 +0900

    💄 AsyncImage 사용

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet20retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet20retrofit/MainActivity.kt
@@ -3,12 +3,19 @@ package kr.ac.kumoh.ce.prof01.jet20retrofit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -16,9 +23,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import kr.ac.kumoh.ce.prof01.jet20retrofit.ui.theme.Jet20RetrofitTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,11 +74,29 @@ fun SongItem(song: Song) {
             .fillMaxWidth(),
         elevation = CardDefaults.cardElevation(8.dp),
     ) {
-        Column(
-            Modifier.padding(8.dp)
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+                .padding(8.dp)
         ) {
-            SongTitle(song.title)
-            SingerName(song.singer)
+            AsyncImage(
+                model = "https://picsum.photos/300/300?random=${song.singer}",
+                contentDescription = "가수 (더미) 이미지",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(percent = 10)),
+            )
+            Spacer(Modifier.size(8.dp))
+            Column(
+                Modifier.padding(8.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceAround,
+            ) {
+                SongTitle(song.title)
+                SingerName(song.singer)
+            }
         }
     }
 }
